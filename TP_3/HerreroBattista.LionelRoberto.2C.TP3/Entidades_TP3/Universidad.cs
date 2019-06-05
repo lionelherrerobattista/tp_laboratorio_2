9d69284e7be0002eceb35947ec8fef5ca422be19
Lionel <lionherrero@gmail.com>
2019-06-05 18:57:04 -0300

    TP_3

--- TP_3/HerreroBattista.LionelRoberto.2C.TP3/Entidades_TP3/Universidad.cs
+++ TP_3/HerreroBattista.LionelRoberto.2C.TP3/Entidades_TP3/Universidad.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,132 +6,142 @@ using System.Threading.Tasks;
 
 namespace Entidades_TP3
 {
-    public class Universidad
+  public class Universidad
+  {
+    public enum EClases
     {
-        public enum EClases
-        {
-            Programacion,
-            Laboratorio,
-            Legislacion,
-            SPD
+      Programacion,
+      Laboratorio,
+      Legislacion,
+      SPD
 
-        }
-
-        private List<Alumno> alumnos;
-        private List<Jornada> jornadas;
-        private List<Profesor> profesores;
-
-        public Universidad()
-        {
-
-        }
-
-        public List<Alumno> Alumnos
-        {
-            get
-            {
-
-            }
-            set
-            {
-
-            }
-        }
-
-        public List<Profesor> Instructores
-        {
-            get
-            {
-
-            }
-            set
-            {
-
-            }
-        }
-
-        public List<Jornada> Jornadas
-        {
-            get
-            {
-
-            }
-            set
-            {
-
-            }
-        }
-
-        public Jornada this[int i]
-        {
-            get
-            {
-
-            }
-            set
-            {
-
-            }
-        }
-
-        public bool Guardar(Universidad uni)
-        {
-
-        }
-
-        private string MostrarDatos(Universidad uni)
-        {
-
-        }
-
-        public override string ToString()
-        {
-            return base.ToString();
-        }
-
-        public static bool operator !=(Universidad g, Alumno a)
-        {
-
-        }
-
-        public static bool operator !=(Universidad g, Profesor i)
-        {
-
-        }
-
-        public static Profesor operator !=(Universidad g, EClases clase)
-        {
-
-        }
-
-        public static Universidad operator +(Universidad g, EClases clase)
-        {
-
-        }
-
-        public static Universidad operator +(Universidad g, Alumno a)
-        {
-
-        }
-
-        public static Universidad operator +(Universidad g, Profesor i)
-        {
-
-        }
-
-        public static bool operator ==(Universidad g, Alumno a)
-        {
-
-        }
-
-        public static bool operator ==(Universidad g, Profesor i)
-        {
-
-        }
-
-        public static Profesor operator ==(Universidad g, EClases clase)
-        {
-
-        }
     }
+
+    private List<Alumno> alumnos;
+    private List<Jornada> jornadas;
+    private List<Profesor> profesores;
+
+    public Universidad()
+    {
+
+    }
+
+    public List<Alumno> Alumnos
+    {
+      get
+      {
+
+      }
+      set
+      {
+
+      }
+    }
+
+    public List<Profesor> Instructores
+    {
+      get
+      {
+
+      }
+      set
+      {
+
+      }
+    }
+
+    public List<Jornada> Jornadas
+    {
+      get
+      {
+
+      }
+      set
+      {
+
+      }
+    }
+
+    public Jornada this[int i]
+    {
+      get
+      {
+        Jornada jornadaAux = null;
+
+        if( i < this.jornadas.Count)
+        {
+          jornadaAux = this.jornadas[i];
+        }
+
+        return jornadaAux;
+      }
+      set
+      {
+        if (i < this.jornadas.Count)
+        {
+          this.jornadas[i] = value;
+        }
+      }
+    }
+
+    public bool Guardar(Universidad uni)
+    {
+
+    }
+
+    private string MostrarDatos(Universidad uni)
+    {
+
+    }
+
+    public override string ToString()
+    {
+      return base.ToString();
+    }
+
+    public static bool operator !=(Universidad g, Alumno a)
+    {
+
+    }
+
+    public static bool operator !=(Universidad g, Profesor i)
+    {
+
+    }
+
+    public static Profesor operator !=(Universidad g, EClases clase)
+    {
+
+    }
+
+    public static Universidad operator +(Universidad g, EClases clase)
+    {
+
+    }
+
+    public static Universidad operator +(Universidad g, Alumno a)
+    {
+
+    }
+
+    public static Universidad operator +(Universidad g, Profesor i)
+    {
+
+    }
+
+    public static bool operator ==(Universidad g, Alumno a)
+    {
+
+    }
+
+    public static bool operator ==(Universidad g, Profesor i)
+    {
+
+    }
+
+    public static Profesor operator ==(Universidad g, EClases clase)
+    {
+
+    }
+  }
 }
